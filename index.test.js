54631e4a084bd9cc601787a06d789273db31c3b5
dyenab <qwpo01010@gmail.com>
2024-12-10 21:46:22 +0900

    my first ci test

---
+++ index.test.js
@@ -0,0 +1,15 @@
+const { sum } = require("../index.js");
+
+test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+});
+test("adds 100 - 50 to equal 50", () => {
+    expect(sum(100, -50)).toBe(50);
+});
+
+test("ads 1 + 2 to equal 3", () => {
+    expect(abs(100, -50)).toBe(3);
+});
+test("adds 100 - 50 to equal 50", () => {
+    expect(adb(-100, -50)).toBe(50);
+});
