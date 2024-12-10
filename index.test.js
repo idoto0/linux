acaa51545a87257170313df5b2893f50f2b532f5
idoto0 <qwpo01010@naver.com>
2024-12-10 23:11:33 +0900

    workflow

--- index.test.js
+++ index.test.js
@@ -6,10 +6,3 @@ test("adds 1 + 2 to equal 3", () => {
 test("adds 100 - 50 to equal 50", () => {
     expect(sum(100, -50)).toBe(50);
 });
-
-test("adds 1 + 2 to equal 3", () => {
-    expect(abs(100, -50)).toBe(3);
-});
-test("adds 100 - 50 to equal 50", () => {
-    expect(abs(-100, -50)).toBe(50);
-});
